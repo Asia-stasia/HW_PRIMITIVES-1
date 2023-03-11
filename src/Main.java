b0b66b1fb96fe58fa86f46cdc84e96005bb1c9ed
ANASTASIA <anastasia83-83@mail.ru>
2023-03-11 14:39:53 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 30303;
+        int bonusMile = ticketPrice / 20;
+        System.out.println(bonusMile + " бонусных миль");
+    }
+}
